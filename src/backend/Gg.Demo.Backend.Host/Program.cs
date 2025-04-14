3380b79e000e0799be41de64c8ca40c3f909ede4
Kirill Eliseev <northwestkir@gmail.com>
2025-04-14 19:50:27 +0300

    kubernetes hosting

--- src/backend/Gg.Demo.Backend.Host/Program.cs
+++ src/backend/Gg.Demo.Backend.Host/Program.cs
@@ -1,12 +1,29 @@
+using System.Net;
 using Gg.Demo.Backend.Core.Matchmaking;
+using Orleans.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseOrleans(static siloBuilder =>
 {
-    siloBuilder.UseLocalhostClustering(serviceId: "backend", clusterId: "dev");
-    siloBuilder.AddMemoryGrainStorage("matchmakingStore");    
+    siloBuilder.UseKubernetesHosting(options => options.ValidateOnStart());
+    
+    siloBuilder.AddMemoryGrainStorage("matchmakingStore");
+    siloBuilder.Configure<EndpointOptions>(options =>
+{
+    // Port to use for silo-to-silo
+    options.SiloPort = 11_111;
+    // Port to use for the gateway
+    options.GatewayPort = 30_000;
+    // IP Address to advertise in the cluster
+    options.AdvertisedIPAddress = IPAddress.Parse("172.16.0.42");
+    // The socket used for client-to-silo will bind to this endpoint
+    options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 40_000);
+    // The socket used by the gateway will bind to this endpoint
+    options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 50_000);
+    });
 });
+
 
 builder.Services.ConfigureOpenMatchFrontEndClient();
 
